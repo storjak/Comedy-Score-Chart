41279288faad84fbe1f32e00bcf702dc6e4b91ec
storjak <65417588+storjak@users.noreply.github.com>
2021-04-27 00:38:05 -0500

    Create sensitive.js
    Sample file

---
+++ back/sensitive.js
@@ -0,0 +1,8 @@
+// THIS IS A SAMPLE FILE - NEVER EXPOSE YOUR PRIVATE ID'S AND SECRETS
+const secrets = {
+    clientID: "Your extension's client ID",
+    clientSecret: Buffer.from("Your extensions's client secret", 'base64'),
+    apiSecret: "Your extension's secret used for OAuth tokens"
+};
+
+module.exports = secrets;
