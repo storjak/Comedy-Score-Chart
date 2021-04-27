72c115c2d32686e4d9e2e584fca887d21e734521
windowpuncher <jakestorkamp@yahoo.com>
2021-04-27 16:07:33 -0500

    testing gitignore stuff

--- back/sensitive.js
+++ back/sensitive.js
@@ -1,6 +1,6 @@
 // THIS IS A SAMPLE FILE - NEVER EXPOSE YOUR PRIVATE ID'S AND SECRETS
 const secrets = {
-    clientID: "Your extension's client ID",
+    clientID: "test",
     clientSecret: Buffer.from("Your extensions's client secret", 'base64'),
     apiSecret: "Your extension's secret used for OAuth tokens"
 };
